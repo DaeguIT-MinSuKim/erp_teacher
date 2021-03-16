45abd3b3df6f277c5382685c45e93aa65a4fb0ca
mskim <net94@naver.com>
2021-03-16 11:14:11 +0900

    사원 상세정보 패널 추가

---
+++ src/main/java/erp_teacher/ui/content/EmployeeDetailPanel.java
@@ -0,0 +1,39 @@
+package erp_teacher.ui.content;
+
+import erp_teacher.dto.EmployeeDetail;
+
+@SuppressWarnings("serial")
+public class EmployeeDetailPanel extends AbstractContentPanel<EmployeeDetail> {
+
+	/**
+	 * Create the panel.
+	 */
+	public EmployeeDetailPanel() {
+
+	}
+
+	@Override
+	public void setItem(EmployeeDetail item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public EmployeeDetail getItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void validCheck() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clearTf() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
